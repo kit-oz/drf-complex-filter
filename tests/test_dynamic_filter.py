6aed1a90b15f781e719d86523f1779a5bdb45c19
Nikita B <nikita.balobanov@gmail.com>
2021-01-15 23:28:58 +0300

    add some missed test cases

---
+++ tests/test_dynamic_filter.py
@@ -0,0 +1,72 @@
+import json
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from rest_framework import status
+from mixer.backend.django import mixer
+
+from .models import TestCaseModel
+
+
+class DynaimcFilterTests(APITestCase):
+    URL = "/test/"
+
+    def setUp(self):
+        user1 = mixer.blend(User, username="user1", password="password1")
+        user2 = mixer.blend(User, username="user2", password="password2")
+        mixer.blend(TestCaseModel, user=user1)
+        mixer.blend(TestCaseModel, user=user2)
+        mixer.blend(TestCaseModel)
+
+        self.user = user1
+
+    def test_me(self):
+        query = {"filters": json.dumps({"type": "operator",
+                             "data": {"attribute": "user",
+                                      "operator": "me",
+                                      "value": ""}})}
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(self.URL, query, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK,
+                         msg=f"Failed on: {query}"
+                             f"\nResponse: {response.data}")
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["user"], self.user.id)
+
+    def test_not_me(self):
+        query = {"filters": json.dumps({"type": "operator",
+                             "data": {"attribute": "user",
+                                      "operator": "not_me",
+                                      "value": ""}})}
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(self.URL, query, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK,
+                         msg=f"Failed on: {query}"
+                             f"\nResponse: {response.data}")
+        self.assertEqual(len(response.data), 2)
+        for result in response.data:
+            self.assertNotEqual(result["user"], self.user.id)
+
+    def test_me_unauthenticated(self):
+        query = {"filters": json.dumps({"type": "operator",
+                             "data": {"attribute": "user",
+                                      "operator": "me",
+                                      "value": ""}})}
+        response = self.client.get(self.URL, query, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK,
+                         msg=f"Failed on: {query}"
+                             f"\nResponse: {response.data}")
+        self.assertEqual(len(response.data), 1)
+        self.assertIsNone(response.data[0]["user"])
+
+    def test_not_me_unauthenticated(self):
+        query = {"filters": json.dumps({"type": "operator",
+                             "data": {"attribute": "user",
+                                      "operator": "not_me",
+                                      "value": ""}})}
+        response = self.client.get(self.URL, query, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK,
+                         msg=f"Failed on: {query}"
+                             f"\nResponse: {response.data}")
+        self.assertEqual(len(response.data), 2)
+        for result in response.data:
+            self.assertIsNotNone(result["user"])
--- tests/test_filter.py
+++ tests/test_filter.py
@@ -1,9 +1,6 @@
-import json
-from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from rest_framework import status
 from parameterized import parameterized
-from mixer.backend.django import mixer
 
 from .models import TestCaseModel
 from .fixtures import RECORDS
@@ -19,7 +16,7 @@ class CommonFilterTests(APITestCase):
             record.save()
 
     @parameterized.expand(TEST_COMMON)
-    def test_filter(self, query, expected_response):
+    def test_common_filter(self, query, expected_response):
         response = self.client.get(self.URL, query, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK,
                          msg=f"Failed on: {query}"
@@ -33,43 +30,3 @@ class CommonFilterTests(APITestCase):
                           msg=f"Failed on: {query}"
                               f"\nResponse: {response.data}"
                               f"\nExpected: {expected_response}")
-
-
-class DynaimcFilterTests(APITestCase):
-    URL = "/test/"
-
-    def setUp(self):
-        user1 = mixer.blend(User, username="user1", password="password1")
-        user2 = mixer.blend(User, username="user2", password="password2")
-        mixer.blend(TestCaseModel, user=user1)
-        mixer.blend(TestCaseModel, user=user2)
-        mixer.blend(TestCaseModel)
-
-        self.user = user1
-
-    def test_me(self):
-        query = {"filters": json.dumps({"type": "operator",
-                             "data": {"attribute": "user",
-                                      "operator": "me",
-                                      "value": ""}})}
-        self.client.force_authenticate(user=self.user)
-        response = self.client.get(self.URL, query, format="json")
-        self.assertEqual(response.status_code, status.HTTP_200_OK,
-                         msg=f"Failed on: {query}"
-                             f"\nResponse: {response.data}")
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]["user"], self.user.id)
-
-    def test_not_me(self):
-        query = {"filters": json.dumps({"type": "operator",
-                             "data": {"attribute": "user",
-                                      "operator": "not_me",
-                                      "value": ""}})}
-        self.client.force_authenticate(user=self.user)
-        response = self.client.get(self.URL, query, format="json")
-        self.assertEqual(response.status_code, status.HTTP_200_OK,
-                         msg=f"Failed on: {query}"
-                             f"\nResponse: {response.data}")
-        self.assertEqual(len(response.data), 2)
-        for result in response.data:
-            self.assertNotEqual(result["user"], self.user.id)
